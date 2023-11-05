515d18fffcffcc66a260d2408f5df50303b5e579
tonib <tonib@lsisa.com>
2023-11-05 19:12:15 +0100

    Changed environment variables for gx dll references

--- LsiGxExtensions-XEv3/EntornoSDKGxEv2.bat
+++
@@ -1,8 +0,0 @@
-REM Establece las variables de entorno para apuntar al sdk de genexus xev2.
-REM Ojo, puede que haya que ejecutar esto como administrador.
-REM Reiniciar el visual studio tras ejecutarlo
-
-SETX GX_SDK_DIR "C:\Program Files (x86)\Artech\GeneXus\GeneXusXEv2PlatformSDK"
-SETX GX_PROGRAM_DIR "C:\Program Files (x86)\ARTech\GeneXus\GeneXusXEv2"
-
-PAUSE
--- SetEnvironmentVariables.bat
+++ SetEnvironmentVariables.bat
@@ -2,7 +2,7 @@ REM Set environment variables to point to genexus xev3
 REM This script needs to be executed as administrator
 REM Restart visual studio after execute this
 
-SETX GX_SDK_DIR "C:\Program Files (x86)\Artech\GeneXus\GeneXusXEv3PlatformSDK"
-SETX GX_PROGRAM_DIR "C:\Program Files (x86)\ARTech\GeneXus\GeneXusXEv3"
+SETX GX_SDK_DIR_EV3 "C:\Program Files (x86)\Artech\GeneXus\GeneXusXEv3PlatformSDK"
+SETX GX_PROGRAM_DIR_EV3 "C:\Program Files (x86)\ARTech\GeneXus\GeneXusXEv3"
 
 PAUSE
