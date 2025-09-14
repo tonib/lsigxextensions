7579cb188e3bf935f67b080aaf7bdd4c7aca1a9f
tonib <tonib@lsisoluciones.com>
2025-09-14 09:04:47 +0200

    Changed powershell scripts execution. Now they are executed as a external process

--- LsiGxExtensions-Gx16/Comandos/Build/Production/ExecuteScript.cs
+++ LsiGxExtensions-Gx16/Comandos/Build/Production/ExecuteScript.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Management.Automation;
 using System.IO;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using LSI.Packages.Extensiones.Utilidades.Threading;
 
 namespace LSI.Packages.Extensiones.Comandos.Build.Production
 {
@@ -19,6 +21,47 @@ namespace LSI.Packages.Extensiones.Comandos.Build.Production
         /// </summary>
         public string ScriptPath;
 
+        // This has stopped running after Windows update, in .NET 3.5 (Gxxev3)
+        //        /// <summary>
+        //        /// Run the script
+        //        /// </summary>
+        //        /// <param name="productionProcess"></param>
+        //        public override void Execute(PrepareProduction productionProcess)
+        //        {
+        //            base.Execute(productionProcess);
+
+        //            productionProcess.LogLine("Executing " + ScriptPath);
+
+        //            using (PowerShell psInstance = PowerShell.Create())
+        //            {
+        //                string script = 
+        //$@"$MODELPATH=""{productionProcess.TargetDirectory}""
+        //$LASTCOPYDESTINATION=""{productionProcess.LastCopyPath}""
+        //$GXENVNAME=""{productionProcess.TargetModel.Name}""
+        //";
+
+        //                script += File.ReadAllText(ScriptPath);
+
+        //                psInstance.AddScript(script);
+
+        //                // invoke execution on the pipeline (collecting output)
+        //                Collection<PSObject> PSOutput = psInstance.Invoke();
+
+        //                // loop through each output object item
+        //                foreach (PSObject outputItem in PSOutput)
+        //                    productionProcess.LogLine(outputItem.ToString());
+
+        //                // Display warnings
+        //                foreach (WarningRecord outputItem in psInstance.Streams.Warning)
+        //                    productionProcess.LogWarningLine(outputItem.ToString());
+
+        //                // Display errors
+        //                foreach (ErrorRecord outputItem in psInstance.Streams.Error)
+        //                    productionProcess.LogErrorLine(outputItem.ToString());
+
+        //            }
+        //        }
+
         /// <summary>
         /// Run the script
         /// </summary>
@@ -29,34 +72,28 @@ namespace LSI.Packages.Extensiones.Comandos.Build.Production
 
             productionProcess.LogLine("Executing " + ScriptPath);
 
-            using (PowerShell psInstance = PowerShell.Create())
-            {
-                string script = 
-$@"$MODELPATH=""{productionProcess.TargetDirectory}""
-$LASTCOPYDESTINATION=""{productionProcess.LastCopyPath}""
-$GXENVNAME=""{productionProcess.TargetModel.Name}""
-";
+            // You can inject variable values with a trick:
+            // powershell.exe -Command "$variable='value'; & 'C:\x\script.ps1'"
+            
+            Dictionary<string, string> varValues = new Dictionary<string, string>();
+            varValues.Add("$MODELPATH", productionProcess.TargetDirectory);
+            varValues.Add("$LASTCOPYDESTINATION", productionProcess.LastCopyPath);
+            varValues.Add("$GXENVNAME", productionProcess.TargetModel.Name);
+            string varAssignment = string.Join(" ; ", varValues.Select(kv => $"{kv.Key}='{kv.Value}'").ToArray());
 
-                script += File.ReadAllText(ScriptPath);
+            // Be sure return zero code if successful, -1 if error (and write error message)
+            string scriptExecution = varAssignment + $" ; & '{ScriptPath}' ; exit 0 ";
+            scriptExecution = $@"try {{ {scriptExecution} }} catch {{ $_ ; exit -1 }} ";
 
-                psInstance.AddScript(script);
-                
-                // invoke execution on the pipeline (collecting output)
-                Collection<PSObject> PSOutput = psInstance.Invoke();
-
-                // loop through each output object item
-                foreach (PSObject outputItem in PSOutput)
-                    productionProcess.LogLine(outputItem.ToString());
-
-                // Display warnings
-                foreach (WarningRecord outputItem in psInstance.Streams.Warning)
-                    productionProcess.LogWarningLine(outputItem.ToString());
-
-                // Display errors
-                foreach (ErrorRecord outputItem in psInstance.Streams.Error)
-                    productionProcess.LogErrorLine(outputItem.ToString());
-                
-            }
+            Process process = new Process();
+            process.StartInfo.FileName = "powershell";
+            process.StartInfo.Arguments = $@"-Command ""{scriptExecution}""";
+            // productionProcess.LogLine($"{process.StartInfo.FileName} {process.StartInfo.Arguments}");
+            int exitCode = ProcessLogger.StartAndLogProcess(process, productionProcess);
+            if(exitCode != 0)
+                productionProcess.LogErrorLine("Powershell execution failed, exit code " + exitCode);
+            else
+                productionProcess.LogLine("Powershell execution succeeded");
         }
 
         public override string ToString()
